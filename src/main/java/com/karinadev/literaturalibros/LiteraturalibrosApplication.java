b87ab6fefe2414bb6eca767fd3e8a305cc8d54a5
Kacri <karina.ghcan@gmail.com>
2024-11-23 11:40:40 -0500

    Se creo el Sprint boot

---
+++ src/main/java/com/karinadev/literaturalibros/LiteraturalibrosApplication.java
@@ -0,0 +1,13 @@
+package com.karinadev.literaturalibros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteraturalibrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiteraturalibrosApplication.class, args);
+	}
+
+}
